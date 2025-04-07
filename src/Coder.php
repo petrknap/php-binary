51b960739a66135209703c1ff7b27abd897c1ad2
Petr Knap <8299754+petrknap@users.noreply.github.com>
2025-04-07 22:12:55 +0200

    feat: `Coder` implements `Stringable`

--- src/Coder.php
+++ src/Coder.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 namespace PetrKnap\Binary;
 
+use Stringable;
+
 /**
  * @internal shared logic
  */
-abstract class Coder
+abstract class Coder implements Stringable
 {
+    /**
+     * @param string $data may contain binary data
+     */
     final public function __construct(
         public readonly string $data = '',
     ) {
@@ -20,7 +25,7 @@ abstract class Coder
     }
 
     /**
-     * @deprecated use readonly property $data
+     * @deprecated use readonly property {@see self::$data}
      */
     final public function getData(): string
     {
@@ -61,4 +66,12 @@ abstract class Coder
      * @throws Coder\Exception\CoderException
      */
     abstract public function zlib(): static;
+
+    /**
+     * @note this is just a helper, this class is not supposed to implement {@see BinariableInterface}
+     */
+    public function __toString(): string
+    {
+        return $this->data;
+    }
 }
